099dc541e137b2fde573f67b1d7b794f5223aed0
Ashfat Al Rashid <61910244+AshfatAbegh@users.noreply.github.com>
2022-04-18 16:46:30 +0600

    Delete librarian directory

--- librarian/view/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$conn = new mysqli('localhost', 'root', '12345', 'project');
-	
-	if(!$conn){
-		die("Error: Can't connect to the database!");
-	}
-?>
--- librarian/view/footer.php
+++
@@ -1,6 +0,0 @@
-<footer align = "center">
-		<h3>Library, Copyright © 2021</h3>
-</footer>
-
-</body>
-</html>
